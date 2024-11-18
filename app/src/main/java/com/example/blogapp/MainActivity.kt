901e63d0b1a86d120e7e94674f2acc68526024ce
Shruti Patil <148354414+Shruti9904@users.noreply.github.com>
2024-11-18 19:45:47 +0530

    Minor change in MainActivity.kt

--- app/src/main/java/com/example/blogapp/MainActivity.kt
+++ app/src/main/java/com/example/blogapp/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.blogapp.ui.theme.BlogAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,7 +39,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BlogApp(){
-    val viewModel = BlogViewModel()
+    val viewModel = viewModel<BlogViewModel>()
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Screen.BlogListScreen.route){
         composable(route= Screen.BlogListScreen.route) {
